a92623f458b876a2378504920167afcdd5f0a446
Sai Karthik S <saikarthik_s@nexteducation.in>
2025-05-16 12:54:05 +0000

    did you give a domain name if so give it

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,18 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
